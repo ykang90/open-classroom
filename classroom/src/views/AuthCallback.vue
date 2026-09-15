f44304d1e274ecdf0dde188d0b92d61d75d6ab8e
ykang90 <yeankang@live.com>
2026-09-15 22:23:25 +0800

    Update AuthCallback.vue

--- classroom/src/views/AuthCallback.vue
+++ classroom/src/views/AuthCallback.vue
@@ -35,6 +35,8 @@ function redirect() {
     router.replace('/onboarding')
   } else if (authStore.isTeacher) {
     router.replace('/teacher')
+  } else if (authStore.isStudent) {
+    router.replace('/student')
   } else {
     router.replace('/')
   }
